1d19a99c4e86e935d29a9f9108e3e376b2956b8f
pierreasd <pierreasd@gmail.com>
2021-10-29 14:40:14 +0700

    Updates

---
+++ precache-manifest.28323bd2f03b5a7d28b5e7390fe26eb2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f853c842de48f51a5d51a887b059a2",
+    "url": "/indograssabadi/index.html"
+  },
+  {
+    "revision": "92e013a31a6c8ac830b2",
+    "url": "/indograssabadi/static/css/main.b6c679a9.chunk.css"
+  },
+  {
+    "revision": "f9029a1b8189f7abd3f9",
+    "url": "/indograssabadi/static/js/2.1a58fb73.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/indograssabadi/static/js/2.1a58fb73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e013a31a6c8ac830b2",
+    "url": "/indograssabadi/static/js/main.477c3b09.chunk.js"
+  },
+  {
+    "revision": "3590d0fce43a82fdd462",
+    "url": "/indograssabadi/static/js/runtime-main.df0bbdbf.js"
+  },
+  {
+    "revision": "2a095324f70ecb06b2ea333ad06cd936",
+    "url": "/indograssabadi/static/media/badm.2a095324.png"
+  },
+  {
+    "revision": "5ffe68db2e18b1b7ba4adf7f49f67309",
+    "url": "/indograssabadi/static/media/baseball.5ffe68db.png"
+  },
+  {
+    "revision": "57b4d5156936aa1cbde9526d1b5fa275",
+    "url": "/indograssabadi/static/media/futsal.57b4d515.png"
+  },
+  {
+    "revision": "6d52e55d1ffd8d822c86f1fe1314c266",
+    "url": "/indograssabadi/static/media/golf.6d52e55d.png"
+  },
+  {
+    "revision": "92933904220e72ba4a3a13449f3840ab",
+    "url": "/indograssabadi/static/media/land.92933904.jpg"
+  },
+  {
+    "revision": "d0d72194a882ffe9fc2f60fde0850edc",
+    "url": "/indograssabadi/static/media/minisoc.d0d72194.png"
+  },
+  {
+    "revision": "a9310b5ba090b41d5e80b6b81f2797c2",
+    "url": "/indograssabadi/static/media/running.a9310b5b.png"
+  },
+  {
+    "revision": "66983f6eed0913e1d6eed22d57477aae",
+    "url": "/indograssabadi/static/media/tennis.66983f6e.png"
+  },
+  {
+    "revision": "0959c39f39e10ec3b51b7ea5c0e75d60",
+    "url": "/indograssabadi/static/media/tmobile.0959c39f.png"
+  },
+  {
+    "revision": "7eb8cb4703c6764c154f5da7691b985d",
+    "url": "/indograssabadi/static/media/vision.7eb8cb47.jpg"
+  }
+]);
